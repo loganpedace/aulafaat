a08732daffc9caf3dcd0d7ea1d3422ddc81eb45f
Danilo8922 <danlenardial@gmail.com>
2024-05-24 18:12:26 -0300

    Danilo

--- Dan.log gaming/javascript/scripts.js
+++ Dan.log gaming/javascript/scripts.js
@@ -12,6 +12,57 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const entradapesquisa = document.getElementById("entradapesquisa");
+  const sugestao = document.getElementById("sugestao");
+
+  // Lista de sugestões
+  const suggestions = [
+      "Assassins_Creed_Brotherhood",
+      "resident-evil-2 Remake",
+      "Grand theft auto 5",
+      "Horizon_Zero_Dawn",
+      "Heavy-rain",
+      "Detroid-Become-human",
+      "The_Walking_Dead,_Season_One",
+  ];
+
+  entradapesquisa.addEventListener("input", function() {
+      const input = entradapesquisa.value.toLowerCase();
+      // Filtra as sugestões com base na entrada do usuário
+      const filteredSuggestions = suggestions.filter(suggestion => 
+          suggestion.toLowerCase().includes(input)
+      );
+
+      // Limpa as sugestões anteriores
+      sugestao.innerHTML = "";
+
+      if (input && filteredSuggestions.length > 0) {
+          // Mostra a lista de sugestões
+          sugestao.style.display = "block";
+
+          filteredSuggestions.forEach(suggestion => {
+              const li = document.createElement("li");
+              li.textContent = suggestion;
+              li.addEventListener("click", function() {
+                entradapesquisa.value = suggestion;
+                  sugestao.style.display = "none";
+              });
+              sugestao.appendChild(li);
+          });
+      } else {
+          // Esconde a lista de sugestões se não houver correspondências
+          sugestao.style.display = "none";
+      }
+  });
+
+  // Esconde a lista de sugestões ao clicar fora dela
+  document.addEventListener("click", function(event) {
+      if (event.target !== entradapesquisa) {
+          sugestao.style.display = "none";
+   }
+ });
+});
 
 
 
